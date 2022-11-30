99a2c7a66db2bcd9a136799296fda5111f2e3798
Jay Gaura <jay.gaura@victorycto.com>
2022-11-30 01:01:06 -0600

    xdebug and no docs

--- docs/home.php
+++
@@ -1,9 +0,0 @@
-<?php declare(strict_types=1); ?>
-
-<h1>Welcome to "Soul Interface"</h1>
-<p>Hi, I am Jay Gaura, an IT-guy, who has also been interested in Vedic philosophy and Bhakti practice. This is sort of a documentation for a little <a href="<?php echo $_ENV['GITHUB_LINK']; ?>" title="on Github">personal fun project</a> I have on <a href="<?php echo $_ENV['GITHUB_LINK']; ?>" title="Github">Github</a>. The idea was to use OOP (object-oriented programming) principles to explain God, haha. Nah, just share some little things I am learning myself.</p>
-<p>Use the top navbar to browse sections. Checkout <a href="<?php echo $_ENV['GITHUB_LINK']; ?>" title="Github">Github</a>, look up the code. Send a feedback.</p>
-<p>Have fun, and thanks for stopping by!</p>
-<p>P.S. I do realise there are gazillions of interpretations of the Vedas out there. I stick to one that has most or all the i's dotted for me. I respect everyone's choices, everyone's methods of worship or what not. This is not to impress, convert or convince. Just a humble and honest sharing of my own views. If it helps a few out there, I will be glad.</p>
-<p>I will appreciate questions and I will be up for a philosophical discussion, but not for arguments. After all, God is one. God is love. Love has no issues with accepting, understanding, reconciling and support.</p>
-<p>&ndash;Peace</p>
